d92de732bf7c766d5d74b22ff322c8b74705709b
Kevin Hetzel <89541507+KHetzel00@users.noreply.github.com>
2025-08-03 18:46:55 -0400

    Added Workflow (#1)

---
+++ API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeAssertions;
+
+namespace API.Tests;
+
+public class UnitTest1
+{
+    private bool banana = true;
+
+    [Fact]
+    public void Test1()
+    {
+        banana.Should().BeTrue();
+    }
+}
